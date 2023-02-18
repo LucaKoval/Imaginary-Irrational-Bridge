60ed3330335824493eacee814f8501a7ac07f94d
Luca Koval <lucakoval@me.com>
2023-02-17 21:02:56 -0500

    Adding Legendre counts

--- quad_gauss_sums.py
+++ quad_gauss_sums.py
@@ -9,13 +9,11 @@ for num in range(3, UPPER, 2):
     	primes.append(num)
 
 for p in primes:
-	legendre_counts = {-1: 0, 0: 0, 1: 0}
 	total_coef = (-1)**((p - 1)/2)
 	root_sum = 0
 	root_sum_string = ''
 	for k in range(p):
 		coef = legendre_symbol(k, p)
-		legendre_counts[coef] += 1
 		if coef == 0:
 			continue
 		op = '+'
@@ -28,5 +26,4 @@ for p in primes:
 		root_sum_string += f' {op} i^({pow_numerator}/{pow_denominator})'
 
 	root_sum_string = root_sum_string[3:]
-	# print(f'sqrt({p}) = {root_sum_string}')
-	print(f'for {p}, legendre_counts: {legendre_counts}')
+	print(f'sqrt({p}) = {root_sum_string}')
