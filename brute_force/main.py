26a01461af551f6edb0eb25c4aa27518c561624b
Luca Koval <lucakoval@me.com>
2023-03-16 23:02:38 -0400

    Adding documentation to brute_force.py

--- brute_force/main.py
+++ brute_force/main.py
@@ -1,13 +1,15 @@
 import random
 import itertools
 
-# sqrtList = [2, 3, 5]
-sqrtList = [5]
+# List of primes for which we wish to find imaginary sum representations
+sqrtList = [2, 3, 5]
 
-# Three total terms
+# Number of terms in the imaginary sum
 total_terms = 3
 
 
+# Helper function that ingests the numbers representing a term in the sum
+# and outputs the corresponding, formatted string
 def to_string(
     coefNumerator, coefDenominator, powerNumerator, powerDenominator, plusMinus
 ):
@@ -21,7 +23,8 @@ def to_string(
     return string
 
 
-# Generate the terms
+# Generate (representations for) all terms/powers of i for which we wish to use
+# in the sums
 terms = set()
 for coefNumerator in range(0, 7):
     for coefDenominator in range(1, 7):
@@ -58,20 +61,34 @@ for coefNumerator in range(0, 7):
                             )
                         )
 
+# Generate all combinations based on the above terms
 combos = itertools.combinations_with_replacement(terms, total_terms)
 
+"""
+Loop through all combinations and evaluate them. If they
+are sufficiently close (within rounding error) to the
+square root of a prime at which we are looking, we have
+found a valid imaginary representation of our prime!
+"""
 for subset in combos:
+    # Add up all the terms in our combination
     total = 0
     for term in subset:
         total += term[0]
 
+    # For each prime we want to examine, check the imaginary representation
     for num in sqrtList:
+        # Check the imaginary representation
         if abs(total.imag) < 1e-10 and abs(pow(num, 1 / 2) - total.real) < 1e-10:
             print(f"Found for the square root of {num}.")
+
+            # Format the string representation of the imaginary representation
             term_string = ""
             for i, term in enumerate(subset):
                 term_string += to_string(*term[1])
                 if i < len(subset) - 1:
                     term_string += " + "
+
+            # Output the string
             print(term_string)
             print()
