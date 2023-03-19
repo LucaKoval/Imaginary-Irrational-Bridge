32a307bef4bb982d73ea478f11c4d48f830f59fc
Luca Koval <lucakoval@me.com>
2023-03-19 14:29:36 -0400

    Updating .gitignore

--- quad_gauss_sums_rust/src/main.rs
+++ quad_gauss_sums_rust/src/main.rs
@@ -59,7 +59,7 @@ fn main() {
         let mut op_string = "".to_string();
         let mut total_coef: i32 = (-1 as i32).pow(((p - 1) / 2) as u32);
         let mut root_sum: i32 = 0;
-        let mut root_sum_string = "".to_string();
+        let mut root_sum_string = String::from("");
 
         // Loop through all integers k < p
         for k in 1..p {
